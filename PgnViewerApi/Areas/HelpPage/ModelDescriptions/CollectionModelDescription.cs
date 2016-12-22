fa1718aacfcfddaa66965f57ea62e78b9503be98
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-12-22 10:02:00 -0800

    initial commit

---
+++ PgnViewerApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PgnViewerApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
